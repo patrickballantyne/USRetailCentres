c9f887360fe0203cc398e6be250a18bb7ccbf1a7
patrick ballantyne <P.J.Ballantyne@liverpool.ac.uk>
2021-03-23 10:01:34 +0000

    tweaking of inputs to both tiers of typology

--- Analysis Code/Retail Centre Typology.R
+++ Analysis Code/Retail Centre Typology.R
@@ -25,7 +25,8 @@ typ <- st_read("output_data/Typology/NE_Typ.gpkg")
 typ_v <- typ %>%
   as.data.frame() %>%
   select(-c(geom)) %>%
-  mutate_if(is.integer, as.numeric)
+  mutate_if(is.integer, as.numeric) %>%
+  mutate_all(~replace(., is.na(.), 0))
 
 typ_s <- as.data.frame(scale(typ_v[, 3:33], center = TRUE, scale = TRUE))
 
@@ -35,6 +36,7 @@ skew <- as.data.frame(sapply(typ_s, function(x) skewness(x)))
 skew <- skew %>%
   rownames_to_column("variable") %>%
   set_names(c("Variable", "Skew"))
+skew
 
 # 3. Checking collinearity of variables -----------------------------------
 
@@ -53,6 +55,13 @@ typ_s <- typ_s %>%
   select(-c(n.hexes, n.bdgs, area, 
             top_category_diversity, pct_Independent, Tot_Res_Count)) %>%
   mutate_all(~replace(., is.na(.), 0))
+
+## Remake corrplot
+cor_m <- cor(typ_s)
+p.mat <- cor.mtest(typ_s)
+corrplot::corrplot(cor_m, type = "upper", tl.srt = 45, method = "number",
+                   p.mat = p.mat, sig.level = 0.01, insig = "blank")
+
 
 # 3. Dimensionality Reduction ---------------------------------------------
 
@@ -101,23 +110,30 @@ fviz_nbclust(typ_s_no_geodemo,  cluster::pam, method = "wss", k.max = 10) +
 # 5. Running Typology -----------------------------------------------------
 
 ## Run typology
-pm <- run_typology(typ_s_no_geodemo, 3)
+pm_2 <- run_typology(typ_s_no_geodemo, 2)
+pm_3 <- run_typology(typ_s_no_geodemo, 3)
 
 ## Plot medoids
-plot_medoids(pm)
+plot_medoids(pm_2)
+plot_medoids(pm_3)
 
 
 # 6. Extracting Results ---------------------------------------------------
 
 ## Extract cluster vals and attach rcID/rcName
-pm_out <- pm[[1]]
+pm_out <- pm_3[[1]]
 pm_out <- cbind(typ[,1:2], pm_out)
+tidy <- pm_out %>% select(rcID, rcName, cluster) %>% dplyr::rename(group_id = cluster)
 #st_write(pm_out, "output_data/Typology/test.gpkg")
+
+## Save medoids
+group_medoids <- plot_medoids(pm_3)
+group_medoids
 
 ## Reattach geodemographic variables
 groups <- cbind(pm_out, geodems)
 
-# 7. Extracting Nested Types ----------------------------------------------
+# 7. Extracting Nested Types (with geodemographics) ----------------------------------------------
 
 ## Get number of distinct groups
 n <- c(unique(groups$cluster))
@@ -128,6 +144,31 @@ types_un <- do.call(rbind, types)
 
 medoids <- lapply(n, function(x) get_nested_types(groups, cl = x, medoids = TRUE))
 medoids_un <- do.call(rbind, medoids)
-plot_type_medoids(medoids_un)
+type_medoids <- plot_type_medoids(medoids_un)
+
+
+group_medoids
+type_medoids
+
+# 7.1 Extracting Nested Types (without geodemographics) -------------------
+
+## Get number of distinct groups
+n <- c(unique(pm_out$cluster))
+
+
+## Apply typology functions across number of groups
+types <- lapply(n, function(x) get_nested_types(pm_out, cl = x, medoids = FALSE))
+types_un <- do.call(rbind, types)
+
+
+## Get medoids
+medoids <- lapply(n, function(x) get_nested_types(pm_out, cl = x, medoids = TRUE))
+medoids_un <- do.call(rbind, medoids)
+type_medoids <- plot_type_medoids(medoids_un)
+type_medoids
+
+# 8. Writing Out ----------------------------------------------------------
+st_write(types_un, "output_data/Typology/NE_RC_Typology.gpkg")
+
 
 
